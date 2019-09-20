6d5d248f4fd48e3f65f08c3597f17ea2b3600481
zjz1993 <1429595365@qq.com>
2019-09-20 15:10:11 +0800

    feat: banner mobile

--- src/components/banner.js
+++ src/components/banner.js
@@ -6,6 +6,9 @@ const BannerWrapper = styled.div`
   width: 100%;
   background-color: #2e49d5;
   padding: 0 10%;
+  @media screen and (max-width: 992px) {
+    padding: 0;
+  }
   .banner-content {
     display: grid;
     grid-template-columns: 1fr 440px;
